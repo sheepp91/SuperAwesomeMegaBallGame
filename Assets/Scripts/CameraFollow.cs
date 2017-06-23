c29519b0e7b1037a55fa9763e76427c2436b6b92
AIE\s171220 <joshua_laurence@live.com>
2017-06-23 10:35:23 +1000

    Player Movement and Camera Follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform player;
+	Vector3 offset;
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	void Update () {
+		Vector3 tempPos = player.transform.position + offset;
+		transform.position = tempPos;
+	}
+}
