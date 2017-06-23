aebc8bf472d78ed946dd48bb0d53119060b54bc1
AIE\s171220 <joshua_laurence@live.com>
2017-06-23 11:12:57 +1000

    Game Reset script updated

--- Assets/Scripts/GameReset.cs
+++ Assets/Scripts/GameReset.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class GameReset : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.CompareTag("Player")) {
+			Application.LoadLevel (Application.loadedLevel);
+		}
 	}
 }
