22d57228702011ff1b09882fa1706b9ac3f8f18d
James da Silva <james.dasilva1111@gmail.com>
2017-06-23 11:31:32 +1000

    + Added Pickup Prefab + Added Reusable Rotate Script

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [TooltipAttribute("Change in degrees per second")]
+    public Vector3 rotation;
+	
+	void Update ()
+    {
+        transform.Rotate(rotation * Time.deltaTime);
+	}
+}
