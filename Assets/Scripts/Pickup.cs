b8cd31d9a186c2c40021fc7e4857f650a571ce32
AIE\s171220 <joshua_laurence@live.com>
2017-06-23 11:44:02 +1000

    Pickup added with destroy script

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.CompareTag ("Player")) {
+			Destroy (gameObject);
+		}
+	}
+}
